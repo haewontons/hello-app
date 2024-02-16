09a2ea9db5916439e50bb75c2004064e398e0fb6
haewontons <160221803+haewontons@users.noreply.github.com>
2024-02-16 23:42:34 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ with st.container():
         st.title("HAPPY BIRTHDAY EVE!")
         st.subheader("An extension to your card...")
         st.write("Using my awesome computer science skills, I've built a small webpage to extend your birthday card coz u "
-             "can't write shit on the small ass panels of the physical card haha. So yk how I was learning how to "
-             "code on Python, I figured out how to like make it into a webpage.")
+             " can't write shit on the small ass panels of the physical card haha.")
  
     
 
@@ -19,7 +18,7 @@ with st.container():
     st.header("A bit about Eve Tse")
         
 st.write("Eve Tse was born on the 17th of February 2008. Born on the year of the rat, and evidently"
-                     " so, she's the sweetest little rodent of all the land <3. As an Aquarious, she certainly"
+                     " so, she's the red:[sweetest] little rodent of all the land <3. As an Aquarious, she certainly"
                      " upholds the clever, analytical, and problem solving traits of the (hot) Water Bearer"
                 )
 st.write("Eve Tse is the most patient and tolerating individual one may ever cross paths with"
@@ -32,5 +31,15 @@ st.write("There is nobody else in the world who can spread good vibes quite like
                  " she sneezes so many fuckn times at once). But most importantly, the whole world turns to look"
                  " when they hear the 'THUCK THUCK THUCK' of the four legged creature hopping around the corner."
                  )
+
+with st.container():
+        st.write("---")
+        st.header('So...')
+        st.write("Have an swagtastic sweet 16th Eve, you are the light of my life, the first person I look for when I walk into a room (actually make that the *second person HAAAHAHA)"
+                " You've kept this crusty old heart beating and given it some of the happiest memories of its life. I can't've wished for a more suited person to share my laughter, tears,"
+                " and gossips with, you are my no.1 fan, Haewon stan. You're my other half, and I'm always listening out for the sound or crutches because I can't live without you my love."
+                " Hip Hip Hooray for Eve's sixteenth, and may the two of us delight in the many more to come <3 "
+                
+        
 
 
