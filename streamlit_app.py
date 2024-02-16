f28df1580030568253109f7a954a05ea8a4a2692
haewontons <160221803+haewontons@users.noreply.github.com>
2024-02-16 23:46:08 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.container():
     st.header("A bit about Eve Tse")
         
 st.write("Eve Tse was born on the 17th of February 2008. Born on the year of the rat, and evidently"
-                     " so, she's the :red[sweetest] little rodent of all the land <3. As an Aquarious, she certainly"
+                     " so, she's the :pink[sweetest] little rodent of all the land <3. As an :blue[Aquarious], she certainly"
                      " upholds the clever, analytical, and problem solving traits of the (hot) Water Bearer"
                 )
 st.write("Eve Tse is the most patient and tolerating individual one may ever cross paths with"
@@ -28,17 +28,17 @@ st.write("Eve Tse is the most patient and tolerating individual one may ever cro
 st.write("There is nobody else in the world who can spread good vibes quite like Eve does."
                  " The whole world turns to look when she laughs (coz she's usually laughing with Haewon and "
                  " Haewon always laughs so godamn loud). The whole world turns to look when she sneezes (coz"
-                 " she sneezes so many fuckn times at once). But most importantly, the whole world turns to look"
-                 " when they hear the 'THUCK THUCK THUCK' of the four legged creature hopping around the corner."
+                 " she sneezes so many :red[fuckn] times at once). But most importantly, the whole world turns to look"
+                 " when they hear the :red['THUCK THUCK THUCK'] of the four legged creature hopping around the corner."
                  )
 
 with st.container():
         st.write("---")
         st.header('So...')
-        st.write("Have an swagtastic sweet 16th Eve, you are the light of my life, the first person I look for when I walk into a room (actually make that the *second person HAAAHAHA)"
+        st.write("Have an swagtastic sweet 16th Eve, you are the :yellow[light] of my life, the first person I look for when I walk into a room (actually make that the *second person HAAAHAHA)"
                 " You've kept this crusty old heart beating and given it some of the happiest memories of its life. I can't've wished for a more suited person to share my laughter, tears,"
                 " and gossips with, you are my no.1 fan, Haewon stan. You're my other half, and I'm always listening out for the sound or crutches because I can't live without you my love."
-                " Hip Hip Hooray for Eve's sixteenth, and may the two of us delight in the many more to come <3 ")
+                " :pink[Hip Hip Hooray] for Eve's sixteenth, and may the two of us delight in the many more to come :red[<3] ")
                 
         
 
