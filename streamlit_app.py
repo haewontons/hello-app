868fb643adf2f0f35bc8a8b876123c6da753126b
haewontons <160221803+haewontons@users.noreply.github.com>
2024-02-16 22:41:26 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,37 +16,22 @@ with st.container():
 # This is eve
 with st.container():
     st.write("---")
-    st.header("This is Eve Tse")
+    st.header("A bit about Eve Tse")
 
-    first_column, second_column = st.columns((2, 1))
-    with first_column:
+
         st.write("Eve Tse was born on the 17th of February 2008. Born on the year of the rat, and evidently"
-                     "so, she's the sweetest little rodent of all the land <3. As an Aquarious, she certainly"
-                     "upholds the clever, analytical, and problem solving traits of the (hot) Water Bearer"
+                     " so, she's the sweetest little rodent of all the land <3. As an Aquarious, she certainly"
+                     " upholds the clever, analytical, and problem solving traits of the (hot) Water Bearer"
                 )
         st.write("Eve Tse is the most patient and tolerating individual one may ever cross paths with"
-                 "in their life. There is none other who will bear to listen to all the same shit every"
-                 "day about the same shitty situation Haewon's found herself in over some shitty person."
+                 " in their life. There is none other who will bear to listen to all the same shit every"
+                 " day about the same shitty situation Haewon's found herself in over some shitty person."
                  "")
         st.write("There is nobody else in the world who can spread good vibes quite like Eve does."
-                 "The whole world turns to look when she laughs (coz she's usually laughing with Haewon and "
-                 "Haewon always laughs so godamn loud). The whole world turns to look when she sneezes (coz"
-                 "she sneezes so many fuckn times at once). But most importantly, the whole world turns to look"
-                 "when they hear the 'THUCK THUCK THUCK' of the four legged creature hopping around the corner."
+                 " The whole world turns to look when she laughs (coz she's usually laughing with Haewon and "
+                 " Haewon always laughs so godamn loud). The whole world turns to look when she sneezes (coz"
+                 " she sneezes so many fuckn times at once). But most importantly, the whole world turns to look"
+                 " when they hear the 'THUCK THUCK THUCK' of the four legged creature hopping around the corner."
                  )
 
 
-    with second_column:
-        st.write('image')
-
-#with st.container():
-#    st.write("---")
- #   st.header("Question")
- #   st.subheader("[Question]")
- #   input = st.text_input("Ansewr below")
- #   if input == ('answer'):
-  #      st.write(':green[Correct]')
-#    elif input == (''):
- #       st.write('')
- #   else:
- #       st.write(':red[Incorrect]')
